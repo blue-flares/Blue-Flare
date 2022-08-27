9cfd09f71ec55dee16c38a21be6acf4eb435b747
Kai Hiwatari <85617535+Hemant-HJ@users.noreply.github.com>
2022-08-27 19:33:16 +0530

    Update button.py

--- utility/button.py
+++ utility/button.py
@@ -22,7 +22,7 @@ class UsernameButton(discord.ui.View):
                 data['username'] = self.username
             else:
                 data = self.ctx.bot.mongo.mongo.datastructure
-                data['username'] = self.username
+                data['username'] = username
             await self.ctx.bot.mongo.update(self.user.id, data)
             self.embed.title = 'Successful Data Update'
             self.embed.description = 'The following data has been updated.'
@@ -39,7 +39,6 @@ class UsernameButton(discord.ui.View):
             await self.ctx.bot.mongo.update(self.user.id, data)
             self.embed.title = 'Successful Data Update'
             self.embed.description = 'Following user\'s data have been reseted.'
-            self.embed.color = discord.Color.green()
             
             await interaction.response.edit_message(embed = self.embed, view = None)
 
@@ -60,58 +59,74 @@ class DonationButton(discord.ui.View):
         self.data = data
         self.mode = mode
         self.embed = embed
-        
-    @discord.ui.button(label = 'Confirm', style = discord.ButtonStyle.success)
+
+    @discord.ui.button(label = 'Confirm', style = discord.ButtonStyle.green)
     async def confirm_button_callback(self, button, interaction):
-        if self.mode == 'add':
-            for user in self.users:
-                data = await self.ctx.bot.mongo.fetch(user)
-                if data:
+        for user in self.users:
+            data = await self.ctx.bot.mongo.fetch(user)
+            if data:
+                if self.mode == 'add':
                     data['donation'] += self.data
-                    await self.ctx.bot.mongo.update(user, data)
-                else:
-                    data = self.ctx.bot.mongo.mongo.datastructure
-                    data['donation'] = self.data
-                    await self.ctx.bot.mongo.update(user, data)
-        
-        elif self.mode == 'remove':
-            for user in self.users:
-                data = await self.ctx.bot.mongo.fetch(user)
-                if data:
+                elif self.mode == 'remove':
                     data['donation'] -= self.data
-                    await self.ctx.bot.mongo.update(user, data)
-                else:
-                    await self.ctx.send(f'<@{user}> have no data entry in the database.')
-        
-        elif self.mode == 'set':
-            for user in self.users:
-                data = await self.ctx.bot.mongo.fetch(user)
-                if data:
+                elif self.mode == 'set':
                     data['donation'] = self.data
-                    await self.ctx.bot.mongo.update(user, data)
-                else:
-                    data = self.ctx.bot.mongo.mongo.datastructure
-                    data['donation'] = self.data
-                    await self.ctx.bot.mongo.update(user, data)
-
-        elif self.mode == 'reset':
-            for user in self.users:
-                data = await self.ctx.bot.mongo.fetch(user)
-                if data:
+                elif self.mode == 'reset':
                     data['donation'] = 0
-                    await self.ctx.bot.mongo.update(user, data)
-                else:
-                    await self.ctx.send(f'<@{user}> have no entry in the database.')
-        
-        else:
-            return
+                await self.ctx.bot.mongo.update(user, data)
 
-        self.embed.color = discord.Color.green()
+            else:
+                await self.ctx.send(f'<@{user}> has no data entry in database. \nPlease add username first.')
+
         self.embed.title = 'Successful Data Update'
-        self.embed.description = 'The following things have been updated.'
+        self.embed.description = 'Following things have been updated'
+        self.embed.color = discord.Color.green()
 
         await interaction.response.edit_message(embed = self.embed, view = None)
     
+    @discord.ui.button(label = 'Cancel', style = discord.ButtonStyle.danger)
+    async def cancel_button_callback(self, button, interaction):
+        await interaction.response.edit_message(content = f'<@{self.ctx.author.id}> process cancelled.', embed = None, view = None)
+
+    async def interaction_check(self, interaction):
+        if interaction.user.id != self.ctx.author.id:
+            return await interaction.response.send_message(content = 'You are not allowed to click on the button.', ephemeral = True)
+        return True
+
+class BattleButton(discord.ui.View):
+    def __init__(self, ctx, users, data, mode, embed, result):
+        super().__init__(timeout=240)
+        self.ctx = ctx
+        self.users = users
+        self.data = data
+        self.embed = embed
+        self.mode = mode
+        self.result = result
+
+    @discord.ui.button(label = 'Confirm', style = discord.ButtonStyle.green)
+    async def confirm_button_callback(self, button, interaction):
+        for user in self.users:
+            data = await self.ctx.bot.mongo.fetch(user)
+            if data:
+                if self.mode == 'add':
+                    data[self.result] += self.data
+                elif self.mode == 'remove':
+                    data[self.result] -= self.data
+                elif self.mode == 'set':
+                    data[self.result] = self.data
+                elif self.mode == 'reset':
+                    data[self.result] = 0
+
+                await self.ctx.bot.mongo.update(user, data)
+            else:
+                await self.ctx.send(f'<@{user}> have no entry in database.\nPlease add the username first.')
+
+        self.embed.color = discord.Color.green()
+        self.embed.title = 'Successful Data Update'
+        self.embed.description = 'Following things will be updated.'
+
+        await interaction.response.edit_message(embed = self.embed, view = None)
+
     @discord.ui.button(label = 'Cancel', style = discord.ButtonStyle.danger)
     async def cancel_button_callback(self, button, interaction):
         await interaction.response.edit_message(content = f'<@{self.ctx.author.id}> process cancelled.', embed = None, view = None)
